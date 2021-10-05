c6d3b7d19762634a08a1671022c78930ab1ecf2e
Christian Sihotang <christian.sihotang23@gmail.com>
2021-10-06 00:13:52 +0700

    Init File

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# donasi-form
+# Donasi Form
+
+This project using :
+  [Material UI / MUI](https://mui.com/getting-started/usage/)
+  [React Final Form](https://final-form.org/docs/react-final-form/getting-started)
+
+[Demo Example](https://frosty-bohr-0b24fe.netlify.app/)
---
+++ src/App.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-alert */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable consistent-return */
+/* eslint-disable space-before-function-paren */
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
